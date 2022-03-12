1304eaab4aea36647951a7e7b08ec318b8102c5d
neklam161 <lyazidlamribah@gmail.com>
2022-03-12 18:59:40 +0100

    program complete

--- Unit Conversion Project/script.js
+++ Unit Conversion Project/script.js
@@ -1,3 +1,22 @@
-function convert(){
-  console.log("testing")
+
+function textshow(val1){
+  let els=document.getElementsByClassName("userinput1");
+  for (var i = 0;i<els.length; i++) {
+    els[i].textContent=val1
+}
+}
+function converter(val){
+  res1=val*3.28084
+  res2=val*0.3048
+  res3=val*0.264172
+  res4=val*3.78541
+  res5=val*2.20462
+  res6=val*0.453592
+  document.getElementById("mettofeet").textContent=res1.toFixed(3)
+  document.getElementById("feettomet").textContent=res2.toFixed(3)
+  document.getElementById("littogal").textContent=res3.toFixed(3)
+  document.getElementById("galtolit").textContent=res4.toFixed(3)
+  document.getElementById("kilotopound").textContent=res5.toFixed(3)
+  document.getElementById("poundtokilo").textContent=res6.toFixed(3)
+  
 }
